dcf0461ea0376e22886cc76e691b5fa8947dd257
Giulio Rossi <ciuffi9@mac.com>
2017-09-29 00:23:43 -0700

    Added connection timestamps.

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express  = require('express');
 var scraper  = require('./scripts/Scraper.js');
 var dbClient = require('./scripts/dbClient.js');
+var moment = require("moment");
 var app      = express();
 var blocked = false;
 var lastTime;
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/HTML/index.html");
+    console.log(moment().format("YYYY:MM:DD:hh:mm:ss A") + "; Site access from: " + req.get("host"));
 });
 app.get('/getLastScrapeTime', function (req, res) {
     if (!blocked){
@@ -34,7 +36,7 @@ app.get('/scrape', function(req, res){
         blocked = true;
         res.sendStatus(202);
         var startTime = new Date().toLocaleString();
-        console.log("Scrape request received from: " + req.get("host") + " for " + (req.query.size || "all") + " departments.");
+        console.log(moment().format("YYYY:MM:DD:hh:mm:ss A") + "; Scrape request received from: " + req.get("host") + " for " + (req.query.size || "all") + " departments.");
         scraper.mine(req.query.size, callback);
         function callback() {
             var endTime = new Date().toLocaleString();
@@ -51,10 +53,12 @@ app.get('/getDepartments', function (req, res) {
     function returnResult(result) {
         res.send(result);
     }
+    console.log(moment().format("YYYY:MM:DD:hh:mm:ss A") + "; All Department Request from: " + req.get("host"));
 });
 
 app.get('/getDepartmentByCode', function (req, res) {
     if (req.query.code){
+        console.log(moment().format("YYYY:MM:DD:hh:mm:ss A") + "; Request for Department by code: " + req.query.code +" from: " + req.get("host"));
         dbClient.getDepartmentByCode(req.query.code, returnResult);
     }else{
         res.send("No code!");
@@ -67,6 +71,7 @@ app.get('/getDepartmentByCode', function (req, res) {
 
 app.get('/getCoursesByCode', function (req, res) {
     if (req.query.code){
+        console.log(moment().format("YYYY:MM:DD:hh:mm:ss A") + "; Request for Courses by code: " + req.query.code +" from: " + req.get("host"));
         dbClient.getCoursesByCode(req.query.code, returnResult);
     }else{
         res.send("No code!")
@@ -79,6 +84,7 @@ app.get('/getCoursesByCode', function (req, res) {
 
 app.get('/getSectionsByCode', function (req, res) {
     if (req.query.code){
+        console.log(moment().format("YYYY:MM:DD:hh:mm:ss A") + "; Request for Sections by code: " + req.query.code +" from: " + req.get("host"));
         dbClient.getSectionsByCode(req.query.code, returnResult);
     }else{
         res.send("No code!")
@@ -87,7 +93,7 @@ app.get('/getSectionsByCode', function (req, res) {
         res.send(result);
     }
 });
-var port = process.env.PORT || 8080;
+var port =  8080;
 app.listen(port);
 
 console.log('Magic happens on port ' + port);
