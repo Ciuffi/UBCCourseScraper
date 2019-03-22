695f8a7fd3bdeb7d20e19e0fd1c12671564b81d7
Giulio Rossi <ciuffi9@mac.com>
2019-03-21 23:34:03 -0700

    Same thing

--- frontend/src/DBHandler.ts
+++ frontend/src/DBHandler.ts
@@ -5,33 +5,32 @@ firebase.initializeApp({
 });
 
 export default class DBHandler {
-  public static yo ='hello';
 
     private static db = firebase.firestore();
 
     public static async GetLastTime() {
       const scrapeDocs = await this.db.collection('scrape_times').get();
-      return scrapeDocs.docs.length !== 0 ? scrapeDocs.docs.pop().data() : 'No results';
+      return scrapeDocs.docs.length !== 0 ? scrapeDocs.docs.pop().data() : {};
     }
 
     public static async getDepartmentByCode(code : string) {
       const dep = await this.db.collection('departments').where('Code', '==', code).get();
       if (dep.size === 0) return null;
-      return dep.docs.length !== 0 ? dep.docs.pop().data() : `No results for ${code}`;
+      return dep.docs.length !== 0 ? dep.docs.pop().data() : {};
     }
 
     public static async getDepartments() {
       const deps = await this.db.collection('departments').get();
-      return deps.docs.length !== 0 ? deps.docs.map(doc => doc.data()) : 'No results';
+      return deps.docs.length !== 0 ? deps.docs.map(doc => doc.data()) : {};
     }
 
     public static async getCoursesByCode(code : string) {
       const course = await this.db.collection('courses').where('Code', '==', code).get();
-      return course.docs.length !== 0 ? course.docs.map(doc => doc.data()) : `No results for ${code}`;
+      return course.docs.length !== 0 ? course.docs.map(doc => doc.data()) : {};
     }
 
     public static async getSectionsByCode(code : string) {
       const section = await this.db.collection('sections').where('Code', '==', code).get();
-      return section.docs.map(doc => doc.data()) ? section.docs != undefined : `No results for ${code}`;
+      return section.docs.map(doc => doc.data()) ? section.docs !== undefined : {};
     }
 }
--- frontend/src/components/ApiTester.vue
+++ frontend/src/components/ApiTester.vue
@@ -1,11 +1,12 @@
 <template>
     <div id='ApiTester'>
+        <b>Last scraped at {{ScrapeTime}}</b><br>
         <input type='text' placeholder='Enter a code!' v-model="Inputcode"></input>
         <button v-on:click="getAllDepartments">getAllDepartments</button>
         <button v-on:click="getDepartmentByCode">getDepartmentByCode</button>
         <button v-on:click="getCourseByCode">getCourseByCode</button>
         <button v-on:click="getSectionByCode">getSectionByCode</button>
-        <pre class="prettyprint lang-json" id="result">{{theData}}</pre>
+        <pre class="prettyprint lang-json" id="result">{{theData !== {} || theData !== null ? theData : `${Inputcode} not found`}}</pre>
     </div>
 </template>
 
@@ -15,18 +16,27 @@ import Handler from '../DBHandler';
 
 @Component
 export default class ApiTester extends Vue {
-    data: object = {};
+    data: firebase.firestore.DocumentData = {};
 
     Inputcode : string = '';
+
+    ScrapeTime : string = '';
 
     mounted() {
       const script = document.createElement('script');
       script.setAttribute('src', 'https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?skin=sunburst');
       document.head.appendChild(script);
+      this.getLastScrapeTime();
     }
 
     get theData() : string {
-      return JSON.stringify(this.data, 0, 4);
+      return JSON.stringify(this.data, null, 4);
+    }
+
+    getLastScrapeTime() {
+      Handler.GetLastTime().then((time) => {
+        this.ScrapeTime = time.end_time;
+      });
     }
 
     getAllDepartments() {
