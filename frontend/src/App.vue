a51bf055dd0f98812fe6da40094b764210ab981e
Giulio Rossi <ciuffi9@mac.com>
2019-03-22 23:46:06 -0700

    add router to frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,20 @@
 <template>
   <div id="app">
     <Welcome></Welcome>
-    <ApiTester></ApiTester>
-    <ApiAccess></ApiAccess>
+    <div id='nav'>
+      <router-link to='/'>Api Tester</router-link>
+      <router-link to='/about'>API Information</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Welcome from './components/Welcome.vue';
-import ApiAccess from './components/ApiAccess.vue';
-import ApiTester from './components/ApiTester.vue';
-
 @Component({
   components: {
     Welcome,
-    ApiAccess,
-    ApiTester,
   },
 })
 export default class App extends Vue {}
@@ -38,5 +36,19 @@ h2 {
 }
 h3 {
     margin: 0px;
+}
+#nav {
+  padding: 10px;
+  text-align: center;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 50px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
